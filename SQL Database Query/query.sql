dce15c754d94b978226a60886ab21303fdc53843
Yyoungion <Yi.du8@education.nsw.gov.au>
2025-12-05 19:28:48 +1100

    Stuf

--- SQL Database Query/query.sql
+++ SQL Database Query/query.sql
@@ -4,8 +4,10 @@ SELECT * FROM RestaurantReservations
 -- Filter reservations to only include those made between 7 PM and 8 PM
 WHERE ReservationTime BETWEEN '19:00:00' AND '20:00:00';
 
--- Query to find all reservations made in the last two weeks
--- It get everything from the "RestaurantReservations" table
-SELECT * FROM RestaurantReservations
--- Filter reservations to only include those made between the 13th and the 25th of September 2024
+-- Query to calculate the total number of guests for reservations made between September 13, 2024, and September 25, 2024
+-- It sums up the "NumberOfGuests" column
+SELECT SUM(NumberOfGuests) AS TotalGuests
+-- From the "RestaurantReservations" table
+FROM RestaurantReservations
+-- Filter reservations to only include those made between September 13, 2024, and September 25, 2024
 WHERE ReservationDate BETWEEN '2024-09-13' AND '2024-09-25';
